ae6959865d5cb799fdd5be6fe81e62bc8ff90309
caitlin chase <caitleavy@gmail.com>
2021-05-13 12:31:23 -0500

    first commit

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Fed1StudioProject
+namespace WebApplication1
 {
     public class Startup
     {
